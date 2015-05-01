6ad55cc421f0140b23c569c6bb3dc14de9f689fb
Jason DuPertuis <gallahad@me.com>
2015-05-01 19:38:43 -0400

    Bridging header

---
+++ FencingRefSwift/FencingRefSwift-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  FencingRefSwift-Bridging-Header.h
+//  FencingRefSwift
+//
+//  Created by Jason DuPertuis on 5/1/15.
+//  Copyright (c) 2015 jdp. All rights reserved.
+//
+
+#import "NSString+FontAwesome.h"
